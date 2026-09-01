a09a2a837589b38683ae4b88fdccebf10194f398
CertifiedMaxxie <lancelotmaxwell256@gmail.com>
2026-09-01 05:04:13 +0300

    fix: remove disableTransitionOnChange to restore smooth theme animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
         >
           <CartProvider>
             {children}
